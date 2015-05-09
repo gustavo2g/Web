c27db1397d1a629c06ad80ce157124dc32e19cc4
gustavo2g <gustavo2g@localhost.localdomain>
2015-05-09 19:29:04 -0430

    Mensaje de prueba

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<script src="js/index.js" type="text/javascript"></script>
+	<title>Chat</title>
+</head>
+<body>
+
+<div id="page1" gg-rol="page">
+	<div gg-rol="header"><h1>Header1</h1></div>
+		<div gg-rol="content">
+		<h1>Inicie sesion:</h1>
+Usuario:
+<input type="text" name="user_login" placeholder="">
+Contraseña
+<input type="text" name="user_login" placeholder="">
+<input type="button" value="Iniciar" name="user_login" placeholder="">
+<input type="button" value="Cancelar" name="user_login" placeholder="">
+
+		</div>
+	<div gg-rol="footer"><h1>footer1</h1></div>
+</div>
+
+
+<div id="page2" gg-rol="page">
+	<div gg-rol="header">Header2</div>
+		<div gg-rol="content">Content2</div>
+	<div gg-rol="footer"><h1>footer2</h1></div>
+</div>
+
+</body>
+</html>
